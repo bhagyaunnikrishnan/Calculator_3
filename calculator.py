9cc7cd8a4ceaf1e3097f04f9c767a9e6ae960694
Bhagya Unnikrishnan <bhagyanandhu2002@gmail.com>
2023-05-09 18:10:22 +0530

    updated

--- calculator.py
+++ calculator.py
@@ -1,95 +1,50 @@
 from tkinter import *
-import math as m
+import math
 window = Tk()
-window.configure(bg="#BFC9CA")
+blank_space = ""
+window.configure(bg="#7F8C8D")
 window.title("Scientific Calculator")
-e = Entry(window, width=50, borderwidth=5, relief=RIDGE, fg="White", bg="#ABEBC6")
-e.grid(row=0, column=0, columnspan=5, padx=10, pady=15)
+window.resizable(width=FALSE, height=False)
+window.geometry("438x573+460+40")
 
-def click(to_print):
-    old = e.get()
-    e.delete(0, END)
-    e.insert(0, old+to_print)
-    return
+coverFrame = Frame(window, bd=20, pady=2, relief=RIDGE)
+coverFrame.grid()
 
-def sc(event):
-    key = event.widget
-    text = key['text']
-    no = e.get()
-    result=''
-    if text=='deg':
-        result=str(m.degrees(float(no)))
-    if text=='sin':
-        result=str(m.sin(float(no)))
-    if text=='cos':
-        result=str(m.cos(float(no)))
-    if text=='tan':
-        result=str(m.tan(float(no)))
-    if text=='log':
-        result=str(m.log10(float(no)))
-    if text=='ln':
-        result=str(m.log(float(no)))
-    if text=='Sqrt':
-        result=str(m.sqrt(float(no)))
-    if text=='x!':
-        result=str(m.factorial(float(no)))
-    if text=='1/x':
-        result=str(1/(float(no)))
-    if text=='pi':
-        if no=="":
-            result=str(m.pi)
-        else:
-            result=str(float(no) * m.pi)
-    if text=='e':
-        if no=="":
-            result=str(m.e)
-        else:
-            result=str(m.e**float(no))
+coverMainFrame = Frame(coverFrame, bd=10, pady=2, bg="Silver", relief=RIDGE)
+coverMainFrame.grid()
 
-    e.delete(0, END)
-    e.insert(0, result)
+MainFrame = Frame(coverMainFrame, bd=5, pady=2, relief=RIDGE)
+MainFrame.grid()
+
+class Calculator():
+    def __init__(self):
+        self.total = 0
+        self.current = ""
+        self.input_value = True
+        self.check_sum = False
+        self.op = ""
+        self.result = False
+
+added_value = Calculator()
+e = Entry(MainFrame, font=('arial', 18, 'bold'), bd=14, width=26, justify="right", bg="#ABEBC6")
+e.grid(row=0, column=0, columnspan=4, pady=1)
+e.insert(0, "0")
+
+numpad = "789456123"
+i = 0
+btn =[]
 
 
+for j in range(3,6):
+    for k in range(3):
+        btn.append(Button(MainFrame, width=6, height=2, font=('arial', 16, 'bold'), bd=4, text=numpad[i]))
+        btn[i].grid(row=j, column=k, pady=1)
+        i += 1
 
-def clear():
-    e.delete(0, END)
-    return
+btnBackSpace = Button(MainFrame, width=6, height=2, font=('arial', 16, 'bold'), bd=4, text="DEL", bg="#FF9800")
+btnBackSpace.grid(row=1, column=0, pady=1)
 
-def bksps():
-    current = e.get()
-    length = len(current)-1
-    e.delete(length, END)
-
-def evaluate():
-    ans = e.get()
-    ans = eval(ans)
-    e.delete(0, END)
-    e.insert(0, ans)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""log = Button(window, text="log", padx=25, pady=10, relief=RIDGE)
-log.bind("<Button-1>", sc)
-log.grid(row=1, column=0)
-
-zero = Button(window, text="0", padx=27, pady=10, bg="Grey", fg="White", command=lambda:click("0"))
-zero.grid(row=7, column=2)"""
-
-
-
-
+btnClear = Button(MainFrame, width=6, height=2, font=('arial', 16, 'bold'), bd=4, text=chr(67), bg="#FF9800")
+btnClear.grid(row=1, column=1, pady=1)
 
 window.mainloop()
